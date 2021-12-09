0801b0a8cd7aad6f6fb142e407b017387c274f70
duyixian <duyixian1234@qq.com>
2021-12-09 14:09:14 +0800

    refactor(main): simple refactor

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,34 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         )
         .get_matches();
 
+    process(&matches).await?;
+    Ok(())
+}
+
+async fn process(matches: &clap::ArgMatches<'_>) -> Result<(), Box<dyn error::Error>> {
+    let url = get_url(matches);
+    println!("Generate .gitignore from {}", url);
+    let content = get_content(url).await?;
+    export(content);
+    Ok(())
+}
+
+fn export(content: String) {
+    let mut file = std::fs::File::create(".gitignore").expect("Failed to create .gitignore");
+    file.write_all(content.as_bytes())
+        .expect("Failed to write to .gitignore");
+}
+
+async fn get_content(url: String) -> Result<String, Box<dyn error::Error>> {
+    let content = reqwest::get(url).await?.text().await?;
+    Ok(content)
+}
+
+fn get_url(matches: &clap::ArgMatches) -> String {
     let features: Vec<&str> = matches.values_of("features").unwrap().clone().collect();
     let url = format!(
         "https://www.toptal.com/developers/gitignore/api/{}",
         features.join(",")
     );
-    println!("Generate .gitignore from {}", url);
-    let content = reqwest::get(url).await?.text().await?;
-    let mut file = std::fs::File::create(".gitignore").expect("Failed to create .gitignore");
-    file.write_all(content.as_bytes())
-        .expect("Failed to write to .gitignore");
-    Ok(())
+    url
 }
